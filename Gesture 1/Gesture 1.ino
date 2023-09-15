d740ae318db7b5c07656467a858034a763259ff9
Mingye <mingyechen521@gmail.com>
2023-09-15 17:40:14 -0400

    Add readme

--- Gesture 1/Gesture 1.ino
+++ Gesture 1/Gesture 1.ino
@@ -1,15 +1,16 @@
+// Pin definitions 
 #define SENSOR1_TRIG_PIN 8
 #define SENSOR1_ECHO_PIN 9
 #define SENSOR2_TRIG_PIN 10
 #define SENSOR2_ECHO_PIN 11
 
-#define DISTANCE_THRESHOLD 30
+#define DISTANCE_THRESHOLD 30 // distance in cm
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600);
+  Serial.begin(9600); // Setup UART/Serial communication
 
-  pinMode(SENSOR1_TRIG_PIN, OUTPUT); // trig pin sensor 1
+  pinMode(SENSOR1_TRIG_PIN, OUTPUT); f// trig pin sensor 1
   pinMode(SENSOR1_ECHO_PIN, INPUT); // echo pin sensor 1
 
   pinMode(SENSOR2_TRIG_PIN, OUTPUT); // trig pin sensor 2
@@ -19,30 +20,32 @@ void setup() {
 
 int GetDistanceFromSensor1()
 {
-  digitalWrite(SENSOR1_TRIG_PIN, LOW);
-  delayMicroseconds(2);
-  digitalWrite(SENSOR1_TRIG_PIN, HIGH);
+  // Start Sensor reading
+  digitalWrite(SENSOR1_TRIG_PIN, LOW); // Set LOW 
+  delayMicroseconds(2); 
+  digitalWrite(SENSOR1_TRIG_PIN, HIGH); // Set HIGH 
   delayMicroseconds(10);
-  digitalWrite(SENSOR1_TRIG_PIN, LOW);
+  digitalWrite(SENSOR1_TRIG_PIN, LOW); // Set LOW
 
-  long duration = pulseIn(SENSOR1_ECHO_PIN, HIGH);
+  long duration = pulseIn(SENSOR1_ECHO_PIN, HIGH); // Measure pulse length
 
-  int distanceCM1 = duration * 0.034/2;
-
+  int distanceCM1 = duration * 0.034/2; // Calculate distance
+ 
   return distanceCM1;
 }
 
 int GetDistanceFromSensor2()
 {
-  digitalWrite(SENSOR2_TRIG_PIN, LOW);
+  // Start Sensor Reading
+  digitalWrite(SENSOR2_TRIG_PIN, LOW); // Set LOW
   delayMicroseconds(2);
-  digitalWrite(SENSOR2_TRIG_PIN, HIGH);
+  digitalWrite(SENSOR2_TRIG_PIN, HIGH); // Set HIGH
   delayMicroseconds(10);
-  digitalWrite(SENSOR2_TRIG_PIN, LOW);
+  digitalWrite(SENSOR2_TRIG_PIN, LOW); // Set LOW
 
-  long duration = pulseIn(SENSOR2_ECHO_PIN, HIGH);
+  long duration = pulseIn(SENSOR2_ECHO_PIN, HIGH); // Measure pulse length
 
-  int distanceCM2 = duration * 0.034/2;
+  int distanceCM2 = duration * 0.034/2; // Calculate distance
 
   return distanceCM2;
 }
@@ -52,6 +55,7 @@ void loop() {
 
   int sensor1Distance = GetDistanceFromSensor1();
   int sensor2Distance = GetDistanceFromSensor2();
+  // Uncomment the code below if you need to see the sensor readings from the serial terminal
   /*Serial.print("sensor 1 data: ");
   Serial.println(sensor1Distance);
 
@@ -63,13 +67,13 @@ void loop() {
   {
     delay(1000);
 
-    while(!(sensor2Distance < DISTANCE_THRESHOLD))
+    while(!(sensor2Distance < DISTANCE_THRESHOLD)) // wait until right sensor detects a hand
     {
-      sensor2Distance = GetDistanceFromSensor2();
+      sensor2Distance = GetDistanceFromSensor2(); 
       delay(20);
     }
 
-    Serial.println("GESTURE DETECTED!!!!!!!");
+    Serial.println("GESTURE DETECTED!!!!!!!"); // Indicate that the gesture has been detected
     delay(2000);
   }
 
---
+++ README.md
@@ -0,0 +1,7 @@
+# Hack the North - Embedded Systems 101 Workshop Code
+
+Here you will find the code used at at the Embedded Systems 101 workshop. 
+
+Gesture 1 folder contains a simple left to right hand gesture recognition.
+
+Questions? Send a message on the Hack the North slack!
